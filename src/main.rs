302b46c942a5731550168790dbc412473d755e6c
phozzy <arthur.fayzullin@gmail.com>
2019-10-26 21:49:12 +0200

    Implement fibonci function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+fn get_position() -> u32 {
+    loop {
+        let mut fibonaci = String::new();
+        println!("Which fibonaci number do you want?");
+        io::stdin().read_line(&mut fibonaci)
+            .expect("Failed to read line");
+        break match fibonaci.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Wrong value! {}", fibonaci);
+                continue;
+            },
+        };
+    }
+}
+fn fibonaci(position: u32, f0: u32, f1: u32) -> u32 {
+    if position == 0 {
+        f0
+    } else if position == 1 {
+        f1
+    } else {
+        fibonaci(position - 1, f1, f0 + f1)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let position = get_position();
+    let fibonaci_number = fibonaci(position, 0, 1);
+    println!("Your fibonaci number is {}!", fibonaci_number);
 }
